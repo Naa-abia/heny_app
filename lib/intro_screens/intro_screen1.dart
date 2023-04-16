d844da21ea1fd41c8ad36db95122c20ccc14f869
augusta <addyauguta@gmail.com>
2023-04-16 23:08:29 +0000

    intro screens created

---
+++ lib/intro_screens/intro_screen1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyIntroPage1 extends StatelessWidget {
+  const MyIntroPage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: const Text("Find amazing experiences  anytime, anywhere")
+
+
+    );
+  }
+}
---
+++ lib/intro_screens/intro_screen2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyIntroPage2 extends StatelessWidget {
+  const MyIntroPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
