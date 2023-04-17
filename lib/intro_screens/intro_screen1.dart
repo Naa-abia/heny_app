f1ad59e5f13fba338461b0ad5ba3c4b471a52d17
augusta <addyauguta@gmail.com>
2023-04-17 00:12:44 +0000

    screen 1 animation

--- lib/intro_screens/intro_screen1.dart
+++ lib/intro_screens/intro_screen1.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class MyIntroPage1 extends StatelessWidget {
   const MyIntroPage1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.yellow,
-        child: const Text("Find amazing experiences  anytime, anywhere"));
+    return Scaffold(
+      body: Center(
+        child: Lottie.network(
+            "https://assets5.lottiefiles.com/private_files/lf30_5i5tlydx.json"),
+      ),
+    );
   }
 }
